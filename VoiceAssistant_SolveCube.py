9e3ef741d58fa2b6553ccc4dd833b7b45d65623e
ShubhamK979 <shubham.kharat@tudip.com>
2024-10-22 11:22:37 +0530

    Updates: - Updated langchain, langchain_community, faiss-cpu, and openai  versions - Replaced RAG integration with RetrievalQA, utilizing a RAG chain. - Updated openAI API call as per new version - Replaced pydub with pygame

--- VoiceAssistant_SolveCube.py
+++ VoiceAssistant_SolveCube.py
@@ -1,28 +1,41 @@
 import os
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 import streamlit as st
 import noisereduce as nr
 from pydub import AudioSegment
 import speech_recognition as sr
 import numpy as np
 import openai
-import pyttsx3
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from gtts import gTTS
+from io import BytesIO
+import pygame 
 
-# Initialize the text-to-speech engine
-engine = pyttsx3.init()
+# initiate pygame
+pygame.mixer.init()
 
-def text_to_speech(text, t):
-    engine.say(text)
-    engine.runAndWait()
-    if not t:
-        engine.stop()
+def text_to_speech(text):
+    # Convert text to speech using gTTS and return the audio as an AudioSegment object
+    tts = gTTS(text, lang='en', slow=False)  # You can change the language by altering the 'lang' argument
+    mp3_fp = BytesIO()  # Use a BytesIO object to store the audio in memory (no saving to disk)
+    tts.write_to_fp(mp3_fp)
+    mp3_fp.seek(0)
+    pygame.mixer.music.load(mp3_fp)
+    return mp3_fp
+
+def play_audio_segment():
+    # Function to play audio, will be run in a separate process
+    pygame.mixer.music.play()
 
 def stop_speech():
-    engine.stop()
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.stop()
+
 
 def preprocess_audio(audio_data):
     audio_array = np.array(audio_data.get_array_of_samples())
@@ -53,44 +66,33 @@ def record_audio():
         processed_audio.export("processed_audio.wav", format="wav")
         st.write("Transcribing audio with Whisper...")
         with open("processed_audio.wav", "rb") as audio_file:
-            transcription = openai.Audio.transcribe(
+            transcription = openai.audio.transcriptions.create(
                 model="whisper-1",
                 file=audio_file
             )
-        
-        return transcription['text']
+        print(transcription)
+        return transcription.text
 
 # Load embeddings and models
 embeddings = OpenAIEmbeddings()
 db = FAISS.load_local("openai_index", embeddings, allow_dangerous_deserialization=True)
-llm = OpenAI(model_name="gpt-4", api_key=os.getenv("OPENAI_API_KEY"))
+llm = ChatOpenAI(model_name="gpt-4o", api_key=os.getenv("OPENAI_API_KEY"))
 
 def rag(query):
-    prompt_template = """
-    Human: Use the following pieces of context to provide a detailed response to the question at the end. 
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    
-    <context>
-    {context}
-    </context>
-
-    Question: {question}
-
-    Assistant:"""
-
-    PROMPT = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
+    system_prompt = '''Your task is to act as our website helper bot. We will ask you any query related to our website 'Solvecube' and you will answer smartly.\n "{context}"'''
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", "{input}"),
+        ]
     )
+    retriever = db.as_retriever()
 
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=db.as_retriever(search_type="similarity", search_kwargs={"k": 10}), 
-        return_source_documents=True,
-        chain_type_kwargs={"prompt": PROMPT}
-    )
-
-    result = qa({"query": query})["result"]
+    # creating rag_chain with history_aware_retriever.. retriever get context from embedded documents based on rephrased query and then add this context to system prompt and then send it to llm
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+    response = rag_chain.invoke({"input": query})
+    result = response["answer"]
 
     if len(result.split()) > 1000:
         result = "The response is too long. Please refine your question or try again."
@@ -100,20 +102,27 @@ def rag(query):
 def main():
     st.title("Solvecube Chatbot")
 
-    text_input = st.text_input(label="", help="Ask here to learn about SolveCube", placeholder="What do you want to know about SolveCube?")
-    
+    text_input = st.text_input(label="Ask here to learn about SolveCube", help="Ask here to learn about SolveCube", placeholder="What do you want to know about SolveCube?")
     if text_input:
-        if "input_field" not in st.session_state or st.session_state.input_field != text_input:
-            response = rag(text_input)
-            st.write(response)
-            text_to_speech(response, True)
+        print(text_input)
+        print(st.session_state)
+        response = rag(text_input)
+        if 'audio_playing' not in st.session_state:
+            st.session_state.audio_playing = True 
+        # Stop any ongoing audio before playing new audio
+        stop_speech()
+        # Play new audio in a separate process
+        text_to_speech(response) 
+        st.markdown(response)
+        play_audio_segment()
 
     if 'audio_playing' not in st.session_state:
         st.session_state.audio_playing = False  # Initialize the state
 
-    stopButton = st.button("Stop Audio", key="stop_audio")
+    stop_button = st.button("Stop Audio", key="stop_audio")
     
-    if stopButton:
+    if st.session_state.stop_audio:
+        print("pressed")
         stop_speech()
         st.session_state.audio_playing = False  # Manage the state
 
@@ -122,8 +131,12 @@ def main():
     if button_pressed:
         transcription = record_audio()
         response = rag(transcription)
-        st.write(response)
-        text_to_speech(response, True)
+        # Stop any ongoing audio before playing new audio
+        stop_speech()
+        # Play new audio in a separate process
+        text_to_speech(response) 
+        st.markdown(response)
+        play_audio_segment()
         st.session_state.audio_playing = True  # Manage the state
 
     # Manage button styling
